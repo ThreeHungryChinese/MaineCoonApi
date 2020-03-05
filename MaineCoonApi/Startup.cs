79ce535918cf565c12a80fcefb2ef9ad6bf107b6
PupilTong <earth.diqiu@gmail.com>
2020-03-04 23:17:35 -0500

    material

--- MaineCoonApi/Startup.cs
+++ MaineCoonApi/Startup.cs
@@ -66,7 +66,7 @@ namespace MaineCoonApi {
                 options.AllowAnyMethod();
             });
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
 
